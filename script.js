b21bfe243e7ea75f7a0be780eee9918e94d4b999
wassim <wbvb41@gmail.com>
2022-10-14 15:58:25 +0200

    solved the second challenge

--- script.js
+++ script.js
@@ -57,6 +57,16 @@ printGoals(...game.scored);
 // 7
 team1 < team2 && console.log('team 1 are more likely to win');
 team1 < team2 || console.log('team 2 are more likely to win');
+// 2-1
 for (let [i, scoredPlayer] of game.scored.entries()) {
   console.log(`Goal ${i + 1}: ${scoredPlayer}`);
 }
+// 2-2
+let average = 0;
+for (let odd of Object.values(game.odds)) {
+  average += odd;
+}
+average /= Object.values(game.odds).length;
+console.log(average);
+// //2-3
+// for (let [])
