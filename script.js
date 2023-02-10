b7b3b16ff91d5116b7916c05d1e95ee84001c20e
wassim <wbvb41@gmail.com>
2023-02-10 17:43:29 +0100

    solved the coding challenge number 3 in the OOP section

--- script.js
+++ script.js
@@ -339,18 +339,6 @@ user.prototype.calcAge = function () {
 
 //OOP challenges
 
-// Coding Challenge #1
-
-/* 
-1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
-2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
-3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
-4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
-DATA CAR 1: 'BMW' going at 120 km/h
-DATA CAR 2: 'Mercedes' going at 95 km/h
-GOOD LUCK 😀
-*/
-
 // const Car = function (make, speed) {
 //   this.speed = speed;
 //   this.make = make;
@@ -373,23 +361,62 @@ GOOD LUCK 😀
 // BMW.brake();
 
 // coding challenge number 2
-class Car {
-  constructor(make, speed) {
-    this.speed = speed;
-    this.make = make;
-  }
-  accelerate() {
-    this.speed = this.speed + 10;
-    console.log(this.speed);
-  }
-  brake() {
-    this.speed = this.speed - 5;
-    console.log(this.speed);
-  }
-  get speedUs() {
-    return this.speed / 1.6;
-  }
-  set speedUs(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+// class Car {
+//   constructor(make, speed) {
+//     this.speed = speed;
+//     this.make = make;
+//   }
+//   accelerate() {
+//     this.speed = this.speed + 10;
+//     console.log(this.speed);
+//   }
+//   brake() {
+//     this.speed = this.speed - 5;
+//     console.log(this.speed);
+//   }
+//   get speedUs() {
+//     return this.speed / 1.6;
+//   }
+//   set speedUs(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+const EV = function (charge, make, speed) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+EV.prototype.accelerate = function () {
+  this.speed += 10;
+  this.charge -= 1;
+  console.log(
+    `Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+const tesla = new EV(23, 'Tesla', 120);
+tesla.chargeBattery(50);
+tesla.accelerate();
