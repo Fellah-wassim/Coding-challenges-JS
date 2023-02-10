4c747bc8962be741c9af1793dd185b966eb0d4c5
wassim <wbvb41@gmail.com>
2023-02-10 15:18:42 +0100

    solved the challenge #1 in OOP section

--- script.js
+++ script.js
@@ -336,4 +336,38 @@ const userObj = new user('wassim', '2003');
 user.prototype.calcAge = function () {
   console.log(2023 - this.birthYear);
 };
-userObj.calcAge();
+
+//OOP challenges
+
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+*/
+
+const Car = function (make, speed) {
+  this.speed = speed;
+  this.make = make;
+};
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(this.speed);
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(this.speed);
+};
+
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+BMW.brake();
